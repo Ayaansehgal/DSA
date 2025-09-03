86a48ad2b207dcc0410a3bccf1a52522c230b139
Ayaansehgal <ayaan05420@gmail.com>
2025-09-03 12:09:37 +0530

    Time: 1154 ms (5.02%) | Memory: 50.5 MB (41.12%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -4,16 +4,14 @@ class Solution {
         int n=nums.length;
         n=n/3;
         for(int i=0;i<nums.length;i++){
-            int count=0;
+            int count=1;
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]==nums[j]){
                     count++;
                 }
             }
-            if(count>=n){
-                if(!res.contains(nums[i])){
-                    res.add(nums[i]);
-                }
+            if (count > n && !res.contains(nums[i])) {
+                res.add(nums[i]);
             }
         }
         return res;
