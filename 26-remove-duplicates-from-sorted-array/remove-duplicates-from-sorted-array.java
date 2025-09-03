b2666e72e56d4bb47620057cf3cf5d2ed4b72116
Ayaansehgal <ayaan05420@gmail.com>
2025-09-03 10:45:27 +0530

    Time: 2 ms (10.49%) | Memory: 45.3 MB (6.70%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],true);
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
