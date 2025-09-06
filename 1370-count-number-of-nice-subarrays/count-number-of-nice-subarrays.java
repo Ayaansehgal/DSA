3e6a112ae32a9411392e7ff01bc7a2585e8d12bf
Ayaansehgal <ayaan05420@gmail.com>
2025-09-06 11:34:29 +0530

    Time: 13 ms (61.76%) | Memory: 55.4 MB (28.18%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -6,7 +6,7 @@ class Solution {
     private int helper(int[] nums, int k) {
         int left = 0, count = 0, res = 0;
         for (int right = 0; right < nums.length; right++) {
-            if (nums[right] % 2 == 1) k--;  // odd
+            if (nums[right] % 2 == 1) k--;  
             while (k < 0) {
                 if (nums[left] % 2 == 1) k++;
                 left++;
