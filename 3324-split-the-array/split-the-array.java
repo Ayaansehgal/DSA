ba391b4ec1e8fc4bf3d965d032efb39e65012a21
Ayaansehgal <ayaan05420@gmail.com>
2025-09-11 18:31:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (84.93%) - LeetSync

--- 3324-split-the-array/split-the-array.java
+++ 3324-split-the-array/split-the-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean isPossibleToSplit(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        int[] freq = new int[101]; 
         for (int x : nums) {
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
-            if (freq.get(x) > 2) {
-                return false; // any number occurs more than 2 times
+            freq[x]++;
+            if (freq[x] > 2) {
+                return false; 
             }
         }
         return true;
