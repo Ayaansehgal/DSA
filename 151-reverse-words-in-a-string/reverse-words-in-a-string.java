58906f445033b4dc9c2785ee3727272199bfa432
Ayaansehgal <ayaan05420@gmail.com>
2026-06-18 14:58:38 +0530

    Time: 7 ms (64.52%) | Memory: 43.8 MB (94.60%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,16 +1,62 @@
 class Solution {
     public String reverseWords(String s) {
-        int i=0;
-        String t=s.trim();
-        String arr[]=t.split("\\s+");
-        int j=arr.length-1;
-        while(i<j){
-            String temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < s.length() && s.charAt(i) == ' ')
+            i++;
+
+        while (j >= 0 && s.charAt(j) == ' ')
+            j--;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (i <= j) {
+            if (s.charAt(i) != ' ') {
+                sb.append(s.charAt(i));
+            } else {
+                if (sb.charAt(sb.length() - 1) != ' ') {
+                    sb.append(' ');
+                }
+            }
+            i++;
+        }
+
+        i = 0;
+        j = sb.length() - 1;
+
+        while (i < j) {
+            char t = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(j));
+            sb.setCharAt(j, t);
             i++;
             j--;
         }
-        return String.join(" ",arr);
+
+        int start = 0;
+        int end = 0;
+
+        while (start < sb.length()) {
+
+            while (end < sb.length() && sb.charAt(end) != ' ')
+                end++;
+
+            int p1 = start;
+            int p2 = end - 1;
+
+            while (p1 < p2) {
+                char t = sb.charAt(p1);
+                sb.setCharAt(p1, sb.charAt(p2));
+                sb.setCharAt(p2, t);
+                p1++;
+                p2--;
+            }
+
+            start = end + 1;
+            end = start;
+        }
+
+        return sb.toString();
     }
 }
