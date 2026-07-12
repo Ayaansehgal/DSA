7784acbdba9512c9b5d23f87ebd8ea9bdce5f11e
Ayaansehgal <ayaan05420@gmail.com>
2026-07-12 15:09:42 +0530

    Time: 76 ms (51.52%) | Memory: 155.1 MB (35.58%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -1,35 +1,58 @@
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
-        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
-        long[] endTimeOfRooms = new long[n];
+
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+
+        PriorityQueue<Integer> available = new PriorityQueue<>();
+
+        PriorityQueue<long[]> occupied = new PriorityQueue<>(
+            (a, b) -> {
+                if (a[0] == b[0])
+                    return Long.compare(a[1], b[1]);
+                return Long.compare(a[0], b[0]);
+            });
+
+        for (int i = 0; i < n; i++)
+            available.offer(i);
+
         int[] count = new int[n];
-        for (int i = 0; i < meetings.length; i++) {
-            int st = meetings[i][0];
-            int et = meetings[i][1];
-            int min = 0;
-            boolean isRoomAllocated = false;
-            for (int room = 0; room < n; room++) {
-                if (st >= endTimeOfRooms[room]) {
-                    endTimeOfRooms[room] = et;
-                    count[room]++;
-                    isRoomAllocated = true;
-                    break;
-                }
-                if (endTimeOfRooms[room] < endTimeOfRooms[min]) {
-                    min = room;
-                }
+
+        for (int[] meeting : meetings) {
+
+            long start = meeting[0];
+            long end = meeting[1];
+            long duration = end - start;
+
+            while (!occupied.isEmpty() && occupied.peek()[0] <= start) {
+                available.offer((int) occupied.poll()[1]);
             }
-            if (!isRoomAllocated) {
-                endTimeOfRooms[min] += (et - st);
-                count[min]++;
+
+            if (!available.isEmpty()) {
+
+                int room = available.poll();
+                count[room]++;
+                occupied.offer(new long[]{end, room});
+
+            } else {
+
+                long[] earliest = occupied.poll();
+
+                long finishTime = earliest[0];
+                int room = (int) earliest[1];
+
+                count[room]++;
+                occupied.offer(new long[]{finishTime + duration, room});
             }
         }
-        int res = 0;
+
+        int ans = 0;
+
         for (int i = 1; i < n; i++) {
-            if (count[i] > count[res]) {
-                res = i;
+            if (count[i] > count[ans]) {
+                ans = i;
             }
         }
-        return res;
+
+        return ans;
     }
 }
