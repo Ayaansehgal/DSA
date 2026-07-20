435f804dc432eb64994a87d37b1bea0f2b39000e
Ayaansehgal <ayaan05420@gmail.com>
2026-07-20 11:38:08 +0530

    Time: 14 ms (70.88%) | Memory: 47.8 MB (32.25%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,43 +1,28 @@
 import java.util.*;
 
-class EleCounter {
-    int val;
-    int freq;
-
-    public EleCounter(int v, int f) {
-        val = v;
-        freq = f;
-    }
-}
-
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int num : nums) {
-            if (map.containsKey(num)) {
-                map.put(num, map.get(num) + 1);
-            } else {
-                map.put(num, 1);
-            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        List<EleCounter> list = new ArrayList<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> pq =
+                new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            list.add(new EleCounter(
-                    entry.getKey(),
-                    entry.getValue()
-            ));
+            pq.offer(entry);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
-
-        Collections.sort(list, (a, b) -> b.freq - a.freq);
-
         int[] res = new int[k];
 
-        for (int i = 0; i < k; i++) {
-            res[i] = list.get(i).val;
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = pq.poll().getKey();
         }
 
         return res;
