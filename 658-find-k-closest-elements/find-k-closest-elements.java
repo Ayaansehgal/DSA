dc1809a4be5498573612749f2289a89bd3b3bb23
Ayaansehgal <ayaan05420@gmail.com>
2026-07-26 11:45:03 +0530

    Time: 12 ms (41.19%) | Memory: 47.9 MB (52.00%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,33 +1,49 @@
 import java.util.*;
 
-class Pair {
-    int val;
-    int diff;
-
-    public Pair(int v, int d) {
-        this.val = v;
-        this.diff = d;
-    }
-}
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
-            if (a.diff != b.diff) {
-                return b.diff - a.diff; 
-            }
-            return b.val - a.val; 
-        });
-        for (int i = 0; i < arr.length; i++) {
-            pq.offer(new Pair(arr[i], Math.abs(arr[i] - x)));
 
-            if (pq.size() > k) {
-                pq.poll();
+        List<Integer> res = new ArrayList<>();
+
+        if (x > arr[arr.length - 1]) {
+            for (int i = arr.length - k; i < arr.length; i++) {
+                res.add(arr[i]);
+            }
+            return res;
+        }
+
+        if (k == arr.length) {
+            for (int i = 0; i < k; i++) {
+                res.add(arr[i]);
+            }
+            return res;
+        }
+
+        int low = 0, high = arr.length;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        int l = low - 1;
+        int r = low;
+
+        while (res.size() < k) {
+            if (l == -1) {
+                res.add(arr[r++]);
+            } else if (r == arr.length) {
+                res.add(arr[l--]);
+            } else if (x - arr[l] <= arr[r] - x) {
+                res.add(arr[l--]);
+            } else {
+                res.add(arr[r++]);
             }
         }
-        List<Integer> res = new ArrayList<>();
-        while (!pq.isEmpty()) {
-            res.add(pq.poll().val);
-        }
+
         Collections.sort(res);
         return res;
     }
