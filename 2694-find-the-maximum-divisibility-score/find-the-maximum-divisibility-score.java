b978d6ad144d75d543562cb741543743cd1f4b0e
Ayaansehgal <ayaan05420@gmail.com>
2026-09-21 23:58:24 +0530

    Time: 1891 ms (5.42%) | Memory: 47.8 MB (7.58%) - LeetSync

--- 2694-find-the-maximum-divisibility-score/find-the-maximum-divisibility-score.java
+++ 2694-find-the-maximum-divisibility-score/find-the-maximum-divisibility-score.java
@@ -1,24 +1,44 @@
-class Solution {
-    public int maxDivScore(int[] nums, int[] divisors) {
-        int best = divisors[0];
-        int maxScore = -1;
+class Solution { 
+    public int maxDivScore(int[] nums, int[] d) { 
 
-        for (int d : divisors) {
-            int score = 0;
+        int best = d[0]; 
+        int maxScore = -1; 
+ 
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+        
+        for (int i = 0; i < d.length; i++) { 
+            map.put(d[i], 0); 
+        } 
 
-            for (int n : nums) {
-                if (n % d == 0) {
-                    score++;
-                }
-            }
+        for (int i = 0; i < nums.length; i++) { 
+
+            for (int j = 1; j * j <= nums[i]; j++) { 
+
+                if (nums[i] % j == 0) { 
+
+                    if (map.containsKey(j)) {
+                        map.put(j, map.get(j) + 1);
+                    }
+
+                    int other = nums[i] / j;
+
+                    if (other != j && map.containsKey(other)) {
+                        map.put(other, map.get(other) + 1);
+                    }
+                } 
+            } 
+        }
+
+        for (int x : d) {
+            int score = map.get(x);
 
             if (score > maxScore || 
-                (score == maxScore && d < best)) {
+                (score == maxScore && x < best)) {
                 maxScore = score;
-                best = d;
+                best = x;
             }
         }
 
         return best;
-    }
+    } 
 }
